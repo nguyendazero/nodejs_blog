247f8d0180c6d66d4ce2d36b0cf9c8be6ed44c0f
nguyendazero <nguyendazero@gmail.com>
2024-02-03 22:16:49 +0700

    Refactor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -8,22 +8,26 @@ const handlebars = require('express-handlebars').create({
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+//XMLHttpRequest, fetch, axios
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+//Routes init
+route(app);
 
 app.listen(port, () => console.log(`Ứng dụng mẫu đang lắng nghe tại http://localhost:${port}`))
